0d15d1eff1cf6c61bb23ce725c474233f5498745
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-07-15 02:58:01 +0700

    adding more things

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -18,4 +18,13 @@ class PlayersController < ApplicationController
             redirect_to "/players/new"
         end
     end
+
+    def show
+        @player = Player.find(params[:id])
+    end
+
+private
+    def player_params
+        params.require(:player).permit(:first_name, :last_name)
+    end
 end
