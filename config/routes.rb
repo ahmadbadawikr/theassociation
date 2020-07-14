16b1395885be99f548827e5f57454a0df4d35551
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-07-15 03:23:30 +0700

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "players/new" => "players#new"
   post "players" => "players#create"
   get "players/:id" => "players#show"
+  get "players/:id/edit" => "players#edit"
+  patch "players/:id" => "players#update"
 end
