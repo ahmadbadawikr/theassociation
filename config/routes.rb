9b6f25ea2cff03d12bbd0cad3b6ee9e1b5a169c7
ahmadbadawikr <ahmadbadawikr@outlook.com>
2020-07-15 03:31:48 +0700

    delete mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root "players#index"
   get "players" => "players#index"
   get "players/new" => "players#new"
   post "players" => "players#create"
   get "players/:id" => "players#show"
   get "players/:id/edit" => "players#edit"
   patch "players/:id" => "players#update"
+  delete "players/:id" => "players#destroy"
 end
